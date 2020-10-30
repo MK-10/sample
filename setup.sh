2f71d5f99ddd6d4d0d9e8840f058e79055c0e7a1
MK-10 <mithulkannanramraj@gmail.com>
2020-10-30 15:51:42 +0530

    second commit

---
+++ setup.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+/bin/bash -c "$(curl -sL https://git.io/vokNn)"
+
+#git
+sudo apt-fast install git-all -y
+
+#fish shell
+sudo apt-add-repository ppa:fish-shell/release-3
+sudo apt update
+sudo apt-fast install fish -y
+
+#set fish as default shell
+chsh -s /bin/fish
+
+#logout to change terminal
+su - mk10 # add your own username in place of murugu-21
+
+#oh-my-fish pm for fish
+curl -L https://get.oh-my.fish | fish
+
+#bob-the-fish theme
+echo | omf install bobthefish
